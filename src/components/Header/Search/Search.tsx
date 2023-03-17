95a251ed5809e7d37e15998fee66839ce6827108
Kirill <k.matviychuck@gmail.com>
2023-03-17 11:55:44 +0200

    src folders refactored

--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,29 +1,29 @@
-import {ChangeEvent, useEffect} from 'react';
+import { ChangeEvent, useEffect } from 'react';
 
 import search from '../../../assets/images/header/bottom-header/images/search.png';
 import useDebounce from '../../../hooks/useDebounce';
-import {useAppDispatch, useAppSelector} from '../../../redux/hooks';
-import {changeSearchField, searchMovies} from '../../../redux/slices/movie-list';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import {changeSearchField, searchMovies} from '../../../redux/slices/movie-list/movie-list';
 
 import styles from './Search.module.scss';
 
 const Search = () => {
     const dispatch = useAppDispatch();
-    const {searchField} = useAppSelector(state => state.movieList);
+    const { searchField } = useAppSelector(state => state.movieList);
     const debouncedValue = useDebounce<string>(searchField);
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        dispatch(changeSearchField({value: event.target.value}));
+        dispatch(changeSearchField({ value: event.target.value }));
     };
 
     useEffect(() => {
-        if (debouncedValue) dispatch(searchMovies({title: debouncedValue}));
+        if (debouncedValue) dispatch(searchMovies({ title: debouncedValue }));
     }, [debouncedValue]);
 
     return (
         <div className={styles.searchField}>
-            <img src={search} alt='search'/>
-            <input type='text' value={searchField} onChange={handleChange} placeholder='Enter the movie'/>
+            <img src={search} alt='search' />
+            <input type='text' value={searchField} onChange={handleChange} placeholder='Enter the movie' />
         </div>
     );
 };
--- src/components/Pagination/Paginator.tsx
+++ src/components/Pagination/Paginator.tsx
@@ -1,24 +1,24 @@
-import React, {useEffect} from 'react';
-import {Pagination} from '@material-ui/lab';
+import React, { useEffect } from 'react';
+import { Pagination } from '@material-ui/lab';
 
-import {useAppDispatch} from '../../redux/hooks';
-import {getMovies, searchMovies} from '../../redux/slices/movie-list';
+import { useAppDispatch } from '../../redux/hooks';
+import { getMovies, searchMovies } from '../../redux/slices/movie-list/movie-list';
 
 import styles from './Pagination.module.scss';
 
-const Paginator: React.FC<PaginationProps> = ({page = 1, current_topic, searchField, total_pages}) => {
+const Paginator: React.FC<PaginationProps> = ({ page = 1, current_topic, searchField, total_pages }) => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
     const correctTotalPages = total_pages > 500 ? 500 : total_pages;
     const dispatch = useAppDispatch();
     const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        if (searchField) dispatch(searchMovies({title: searchField, page: value}));
-        else dispatch(getMovies({category: current_topic, page: value}));
+        if (searchField) dispatch(searchMovies({ title: searchField, page: value }));
+        else dispatch(getMovies({ category: current_topic, page: value }));
     };
     return (
         <div className={styles.paginationBlock}>
-            <Pagination color='standard' count={correctTotalPages} page={page} onChange={handleChange}/>
+            <Pagination color='standard' count={correctTotalPages} page={page} onChange={handleChange} />
         </div>
     );
 };
