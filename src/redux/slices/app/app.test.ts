3b6a9c12b32eea42a7267a14f6c9b30fd0bb0378
Kirill <k.matviychuck@gmail.com>
2023-03-17 13:38:14 +0200

    added test in redux => appslice component

--- src/redux/slices/app/app.test.ts
+++ src/redux/slices/app/app.test.ts
@@ -1,7 +1,16 @@
-import {appSlice} from './app';
+import { appReducer, AppReducerType, setAppProgressStatus } from './app';
 
 describe('app tests', () => {
-    test('status changes correctly', () => {
+    const initialState: AppReducerType = {
+        status: 'idle'
+    }
+    beforeEach(() => {
+        initialState.status = 'idle'
+    })
 
-    });
+    test('should return the initial state', () => {
+        const action = setAppProgressStatus({ status: 'loading' })
+        const finalState = appReducer(initialState, action)
+        expect(finalState.status).toBe('loading')
+    })
 });
--- src/redux/slices/app/app.ts
+++ src/redux/slices/app/app.ts
@@ -1,4 +1,4 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const initialState: AppReducerType = {
     status: 'idle'
@@ -8,15 +8,15 @@ export const appSlice = createSlice({
     name: 'app',
     initialState: initialState,
     reducers: {
-        setAppProgressStatus(state, action: PayloadAction<{status: AppProgressStatusType}>) {
+        setAppProgressStatus(state, action: PayloadAction<{ status: AppProgressStatusType }>) {
             state.status = action.payload.status;
         }
     },
 });
 
-export const {setAppProgressStatus} = appSlice.actions;
+export const { setAppProgressStatus } = appSlice.actions;
 
-type AppReducerType = {
+export type AppReducerType = {
     status: AppProgressStatusType
 }
 export type AppProgressStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
